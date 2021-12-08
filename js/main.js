d2447aa7e93b06dd182227e3ae34842e3b9631b8
Nikolay Vinogradov <nikolay.vin93@gmail.com>
2021-12-08 16:20:23 +0300

    create: modal window added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
+$(document).ready(function(){
 var menuBurger = document.querySelector('.navbar-burger');
   menuBurger.addEventListener("click", function(){
   document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+});
+
+
+var modalButton = $('[data-toggle=modal]');
+var modalClose = $('.modal__close');
+  modalButton.on('click', openModal);
+  modalClose.on('click', closeModal);
+
+function openModal() {
+  var modalWindow = $('.modal');
+  var modalBg = $('.modal__bg');
+  modalWindow.addClass('modal--active');
+  modalBg.addClass('modal__bg--active');
+}
+
+function closeModal(event) {
+  event.preventDefault();
+  var modalWindow = $('.modal');
+  var modalBg = $('.modal__bg');
+  modalWindow.removeClass('modal--active');
+  modalBg.removeClass('modal__bg--active');
+}
 });
